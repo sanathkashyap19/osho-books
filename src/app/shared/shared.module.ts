0b7dfb2b35b1cc49f2e314f7965e033237d27ae0
Sanath Kashyap <sanathkashyap92@gmail.com>
2020-06-05 17:14:50 +0530

    Header (#3)
    * created carousel and item cards
    
    * added folder structure for universal css files
    
    * header skeleton
    
    * basic landing page skeleton done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { CarouselComponent } from './carousel/carousel.component';
 import { ItemCardComponent } from './item-card/item-card.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [CarouselComponent, ItemCardComponent],
+  declarations: [CarouselComponent, ItemCardComponent, HeaderComponent],
   imports: [CommonModule, NgbModule],
-  exports: [NgbModule, CarouselComponent, ItemCardComponent],
+  exports: [NgbModule, CarouselComponent, ItemCardComponent, HeaderComponent],
 })
 export class SharedModule {}
