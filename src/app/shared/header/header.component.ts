36021f65d9490dc55dad373e8e9dd83ab7ee893e
Veerapandian <Veera@MacBook-Pro.local>
2020-06-22 20:03:08 +0530

    Added hamburger menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostListener } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isCollapsed: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    if(!event.target.classList.contains('navbar-collapse *') && !event.target.classList.contains('navbar-toggler') && !event.target.classList.contains('icon-bar')) {
+      if(this.isCollapsed) {
+        this.isCollapsed = !this.isCollapsed;
+      }
+      
+    } 
   }
 
 }
