71e398cca22bd4fc5ed808b6166e6cf0cbd5bf39
Veerapandian <Veera@MacBook-Pro.local>
2020-06-23 07:53:35 +0530

    Added routing to the hamburger menus

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,18 +8,26 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class HeaderComponent implements OnInit {
   isCollapsed: boolean;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   
+  navigateFlow(page: string) {
+    if(page === 'home') {
+      this.router.navigate(['../']);
+    }
+    if(page === 'products') {
+      this.router.navigate(['/product-details']);
+    }
+  }
+
+  //clicking outside should close the hamburger menu
   @HostListener('document:click', ['$event'])
-  clickout(event) {
+  clickout(event: any) {
     if(!event.target.classList.contains('navbar-collapse *') && !event.target.classList.contains('navbar-toggler') && !event.target.classList.contains('icon-bar')) {
       if(this.isCollapsed) {
         this.isCollapsed = !this.isCollapsed;
       }
-      
     } 
   }
 
