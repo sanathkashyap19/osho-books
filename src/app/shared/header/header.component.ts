a5b7e266d31ad901ffd9b6e07007232831063b05
Veerapandian <Veera@MacBook-Pro.local>
2020-06-23 17:11:59 +0530

    updated routing in hamburger menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,19 +7,10 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   isCollapsed: boolean;
 
-  constructor(private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {}
   
-  navigateFlow(page: string) {
-    if(page === 'home') {
-      this.router.navigate(['../']);
-    }
-    if(page === 'products') {
-      this.router.navigate(['/product-details']);
-    }
-  }
-
   //clicking outside should close the hamburger menu
   @HostListener('document:click', ['$event'])
   clickout(event: any) {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { CarouselComponent } from './carousel/carousel.component';
 import { ItemCardComponent } from './item-card/item-card.component';
 import { HeaderComponent } from './header/header.component';
+import { PagesRoutingModule } from '../pages/pages-routing.module';
 
 @NgModule({
   declarations: [CarouselComponent, ItemCardComponent, HeaderComponent],
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, PagesRoutingModule],
   exports: [NgbModule, CarouselComponent, ItemCardComponent, HeaderComponent],
 })
 export class SharedModule {}
