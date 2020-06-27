774ae10d5fa16a905b05c945dd66b32e42314ecf
sanathkashyap19 <sanathkashyap92@gmail.com>
2020-06-27 17:32:45 +0530

    added checkout table

---
+++ src/app/checkout/checkout-table/checkout-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-table',
+  templateUrl: './checkout-table.component.html',
+  styleUrls: ['./checkout-table.component.scss']
+})
+export class CheckoutTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/checkout/checkout.module.ts
+++ src/app/checkout/checkout.module.ts
@@ -2,12 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutTableComponent } from './checkout-table/checkout-table.component';
 
-
-
 @NgModule({
   declarations: [CheckoutTableComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [CheckoutTableComponent],
 })
-export class CheckoutModule { }
+export class CheckoutModule {}
