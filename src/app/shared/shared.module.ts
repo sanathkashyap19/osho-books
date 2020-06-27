b8ffa4bed5b628ed6d5c7ade037bdbdea0f9a7a0
sanathkashyap19 <sanathkashyap92@gmail.com>
2020-06-27 13:14:29 +0530

    cosmetic and alignment changes for demo

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ItemCardComponent } from './item-card/item-card.component';
 import { HeaderComponent } from './header/header.component';
+import { PagesRoutingModule } from '../pages/pages-routing.module';
 
 @NgModule({
   declarations: [CarouselComponent, ItemCardComponent, HeaderComponent],
-  imports: [CommonModule, NgbModule],
+  imports: [CommonModule, NgbModule, PagesRoutingModule],
   exports: [NgbModule, CarouselComponent, ItemCardComponent, HeaderComponent],
 })
 export class SharedModule {}
