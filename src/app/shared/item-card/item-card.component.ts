f355fb5b0380af9a6201801281a88b1caa040e3c
Sanath Kashyap <sanathkashyap92@gmail.com>
2020-06-27 13:15:35 +0530

    Demo fixes (#6)
    * created module and page
    
    * cosmetic and alignment changes for demo

--- src/app/shared/item-card/item-card.component.ts
+++ src/app/shared/item-card/item-card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ItemCardComponent implements OnInit {
   @Input() image;
   @Input() title;
-  @Input() text;
+  @Input() price;
 
   constructor() {}
 
