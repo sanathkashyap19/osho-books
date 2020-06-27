fa4b6c2e03c8c94b3bee21a3849f148da4960249
Veerapandian <Veera@MacBook-Pro.local>
2020-06-27 20:56:09 +0530

    Added routing to the header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LandingComponent } from './pages/landing/landing.component';
-import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./pages/pages.module').then((m) => m.PagesModule),
-  },
+    loadChildren: () => import('./pages/pages.module').then((m) => m.PagesModule),
+  }
 ];
 
 @NgModule({
