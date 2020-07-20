f80892b8b551e1c164c5980990817338f227a520
sanathkashyap19 <sanathkashyap92@gmail.com>
2020-07-20 22:25:20 +0530

    removed generic tabs

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,22 +4,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarouselComponent } from './carousel/carousel.component';
 import { ItemCardComponent } from './item-card/item-card.component';
 import { HeaderComponent } from './header/header.component';
-import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
-  declarations: [
-    CarouselComponent,
-    ItemCardComponent,
-    HeaderComponent,
-    TabsComponent,
-  ],
+  declarations: [CarouselComponent, ItemCardComponent, HeaderComponent],
   imports: [CommonModule, NgbModule],
-  exports: [
-    NgbModule,
-    CarouselComponent,
-    ItemCardComponent,
-    HeaderComponent,
-    TabsComponent,
-  ],
+  exports: [NgbModule, CarouselComponent, ItemCardComponent, HeaderComponent],
 })
 export class SharedModule {}
