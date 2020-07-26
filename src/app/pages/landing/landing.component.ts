800039c8dbb059ee34205f0fb1694a3251fde98b
Veerapandian <Veera@MacBook-Pro.local>
2020-07-26 12:48:08 +0530

    updated auth

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -8,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class LandingComponent implements OnInit {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   imagesUrl;
+  public currentUser;
 
-  constructor() {}
+  constructor() {
+    this.currentUser = localStorage.getItem('currentUser')? JSON.parse(localStorage.getItem('currentUser')) : '';
+  }
+
+  // constructor() {}
 
   ngOnInit(): void {
+    console.log(this.currentUser,"hi")
     this.imagesUrl = [
       '../../../assets/images/Capture.PNG',
       '../../../assets/images/Capture.PNG',
