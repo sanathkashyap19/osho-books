57849200e1545fa7fc14fd853abc1953e709c096
sanathkashyap19 <sanathkashyap92@gmail.com>
2020-08-06 15:16:01 +0530

    demo fixes

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VerticalTabsComponent } from './vertical-tabs/vertical-tabs.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [VerticalTabsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [VerticalTabsComponent],
 })
 export class UserProfileModule {}
