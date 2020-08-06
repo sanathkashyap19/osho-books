267bf5d94a5835c339577ef61f61f78687055ceb
Sanath Kashyap <sanathkashyap92@gmail.com>
2020-08-06 15:18:12 +0530

    User profile (#17)
    * created module
    
    * built basic tab component with vertical and horizontal orientation
    
    * tabs styling
    
    * removed generic tabs
    
    * minor fixes
    
    * demo fixes

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { VerticalTabsComponent } from './vertical-tabs/vertical-tabs.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [],
+  declarations: [VerticalTabsComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [VerticalTabsComponent],
 })
 export class UserProfileModule {}
