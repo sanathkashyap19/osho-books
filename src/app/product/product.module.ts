2e29166630dc93e97db3399787d6f9b5bfc51b20
Veerapandian <Veera@MacBook-Pro.local>
2020-09-02 17:58:18 +0530

    comments and relevant product section added

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -4,10 +4,12 @@ import { SliderModule } from 'angular-image-slider';
 import { ProductImageSliderComponent } from './product-image-slider/product-image-slider.component';
 import { ProductInfoComponent } from './product-info/product-info.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RelevantProductsComponent } from './relevant-products/relevant-products.component';
+import { CommentsSectionComponent } from './comments-section/comments-section.component';
 
 @NgModule({
-  declarations: [ProductImageSliderComponent, ProductInfoComponent],
+  declarations: [ProductImageSliderComponent, ProductInfoComponent, CommentsSectionComponent, RelevantProductsComponent],
   imports: [CommonModule, SliderModule, CarouselModule],
-  exports: [ProductImageSliderComponent, ProductInfoComponent],
+  exports: [ProductImageSliderComponent, ProductInfoComponent, CommentsSectionComponent, RelevantProductsComponent],
 })
 export class ProductModule {}
---
+++ src/app/product/relevant-products/relevant-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-relevant-products',
+  templateUrl: './relevant-products.component.html',
+  styleUrls: ['./relevant-products.component.scss']
+})
+export class RelevantProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
