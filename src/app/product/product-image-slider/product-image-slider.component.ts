9b06e8ca0d56b4a156f6c452c719823c457529e4
VEERAPANDIAN <31968060+Veerapandian98@users.noreply.github.com>
2020-09-02 17:54:25 +0530

    owl-carousel for details page (#19)
    Co-authored-by: Veerapandian <Veera@MacBook-Pro.local>

--- src/app/product/product-image-slider/product-image-slider.component.ts
+++ src/app/product/product-image-slider/product-image-slider.component.ts
@@ -1,14 +1,37 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-product-image-slider',
   templateUrl: './product-image-slider.component.html',
-  styleUrls: ['./product-image-slider.component.scss'],
+  styleUrls: ['./product-image-slider.component.scss']
 })
 export class ProductImageSliderComponent implements OnInit {
   @Input() imagesUrl;
 
   constructor() {}
+
+  customOptions: OwlOptions = {
+    loop: false,
+    dots: false,
+    navSpeed: 700,
+    navText: [ '<i class="material-icons">chevron_left</i>', '<i class="material-icons">chevron_right</i>' ],
+    responsive: {
+      0: {
+        items: 1,
+        margin: 5
+      },
+      400: {
+        items: 2,
+        margin: 5
+      },
+      740: {
+        items: 2,
+        margin: 10
+      }
+    },
+    nav: true
+  }
 
   ngOnInit(): void {}
 }
