454ca083a80ab076f8fa3db4e5180e4d449add0e
Veerapandian <Veera@MacBook-Pro.local>
2020-09-05 10:52:31 +0530

    Updated star rating

--- src/app/shared/components/star-rating/star-rating.component.ts
+++ src/app/shared/components/star-rating/star-rating.component.ts
@@ -5,44 +5,26 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './star-rating.component.html',
   styleUrls: ['./star-rating.component.scss']
 })
-export class StarRatingComponent implements OnInit {
-  starList: boolean[] = [true, true, true, true, true];       // create a list which contains status of 5 stars
-  rating = 0;
-  constructor() { }
+export class StarRatingComponent {
+  stars: number[] = [1, 2, 3, 4, 5];
+  selectedValue: number = 0;
 
-  ngOnInit(): void {
+  countStar(star) {
+    this.selectedValue = star;
   }
 
-  setStarTable(index: any, status): any {
-    if (status) {
-      this.rating = index + 1;
-      switch (index) {
-        case 0:
-          return this.starList[index] = false;
-        case 1:
-          return this.starList = [false, false, true, true, true];
-        case 2:
-          return this.starList = [false, false, false, true, true];
-        case 3:
-          return this.starList = [false, false, false, false, true];
-        case 4:
-          return this.starList = [false, false, false, false, false];
-      }
-      return null;
-    } else {
-      this.rating = index;
-      switch (index) {
-        case 0:
-          return this.starList = [true, true, true, true, true];
-        case 1:
-          return this.starList = [false, true, true, true, true];
-        case 2:
-          return this.starList = [false, false, true, true, true];
-        case 3:
-          return this.starList = [false, false, false, true, true];
-        case 4:
-          return this.starList[index] = true;
-      }
+  addClass(star) {
+    let ab = "";
+    for (let i = 0; i < star; i++) {
+      ab = "starId" + i;
+      document.getElementById(ab).classList.add("selected");
+    }
+  }
+  removeClass(star) {
+    let ab = "";
+    for (let i = star-1; i >= this.selectedValue; i--) {
+      ab = "starId" + i;
+      document.getElementById(ab).classList.remove("selected");
     }
   }
 }
